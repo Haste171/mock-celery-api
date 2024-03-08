98cd12d5d6f49d3dc15eae27f787bfef715ee280
Your Name <34923485+Haste171@users.noreply.github.com>
2024-03-08 10:16:27 -0600

    Update

--- tasks.py
+++ tasks.py
@@ -1,22 +1,22 @@
 import time
 from celery import Celery
-from decouple import config
-REDIS_URL = config('REDIS_URL')
+# from decouple import config
+# REDIS_URL = config('REDIS_URL')
 
 # For localhost
-# app = Celery(
-#             'my_app', 
-#              broker='redis://localhost:6379/0', 
-#              backend='redis://localhost:6379/0', 
-#              broker_connection_retry_on_startup=True
-#)
-
 app = Celery(
             'my_app', 
-             broker=f'{REDIS_URL}?ssl_cert_reqs=CERT_NONE', 
-             backend=f'{REDIS_URL}?ssl_cert_reqs=CERT_NONE', 
+             broker='redis://localhost:6379', 
+             backend='redis://localhost:6379', 
              broker_connection_retry_on_startup=True
 )
+
+# app = Celery(
+#             'my_app', 
+#              broker=f'{REDIS_URL}?ssl_cert_reqs=CERT_NONE', 
+#              backend=f'{REDIS_URL}?ssl_cert_reqs=CERT_NONE', 
+#              broker_connection_retry_on_startup=True
+# )
 
 @app.task
 def add_numbers(arg1, arg2):
